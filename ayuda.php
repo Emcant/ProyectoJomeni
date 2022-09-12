383d6c2e86c6bd93a77cbd84a56ef007b96493de
Emcant <emcant342@gmail.com>
2022-09-12 16:37:05 -0500

    V2

---
+++ ayuda.php
@@ -0,0 +1,10 @@
+<?php
+
+include 'templates/header.php';
+?>
+<div class="container h-100">
+    <h1>Centro de Ayuda</h1>
+</div>
+<?php
+include 'templates/footer.php';
+?>
---
+++ bolsa.php
@@ -0,0 +1,10 @@
+<?php
+
+include 'templates/header.php';
+?>
+<div class="container h-100">
+    <h1>Bolsa Inmobiliaria</h1>
+</div>
+<?php
+include 'templates/footer.php';
+?>
